2c8d0652bb4585997a9f8d3348b7269c59cfb4c3
Taylor Ambrose <taykesby@gmail.com>
2023-03-27 11:52:08 -0400

    fixed imports

--- lib/db/course.py
+++ lib/db/course.py
@@ -2,7 +2,10 @@ from .gradebook import Gradebook
 
 
 class Course:
+    all = []
+
     def __init__(self, name, level, credits):
         self.name = name
         self.level = level
         self.credits = credits
+        Course.all.append(self)
--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,11 @@
 import ipdb
-from lib.db import *
+from db import *
 
 # Course
 # def __init__(self, name, level, course_credit):
 
-bio = Course("Biology", 1000, 3)
+bio = Course("Biology 101", 1000, 4)
+his = Course("History 101", 1000, 3)
 
 # Student
 # def __init__(self, first_name, last_name):
