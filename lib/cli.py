5424dc2350d5b383830ad4f99b56bba73bdd8c78
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-03-28 12:02:32 -0400

    example of cli

---
+++ lib/cli.py
@@ -0,0 +1,8 @@
+meowmeow = "lol check it out we are wizards now"
+
+while meowmeow != 'x':
+    meowmeow = input(':upside_down_face:')
+    if meowmeow == 'l':
+        print('we love python')
+    elif meowmeow != 'x':
+        print(f"{meowmeow} is not the exit")
