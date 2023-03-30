e38a093b4e714e2c0f2711c371e656d76e36c800
Taylor Ambrose <taykesby@gmail.com>
2023-03-30 10:34:17 -0400

    file clean up

--- lib/seed.py
+++ lib/seed.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import sessionmaker
 from db.models import (Base, Course, Student)
 
 
-# This is all commented out because the databases have already been seeded and tables created.
-# The code here should not need run unless creating new tables and/or seeding with data.
+# **This is all commented out because the databases have already been seeded and tables created.**
+# **The code here should not need run unless creating new tables and/or seeding with data.**
 
 # if __name__ == '__main__':
 #     engine = create_engine('sqlite:///models.db')
